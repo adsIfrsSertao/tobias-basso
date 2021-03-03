3cafabffc2ca147f1d8efba7ec33db035eb4166f
Tobias <tobiasbasso@hotmail.com>
2021-03-03 13:46:36 -0300

    \

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>teste</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+            <h1>@yield('cabecalho')</h1>
+            <h5 class="my-0 mr-md-auto font-weight-normal">LaDiversité</h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+            </nav>
+            <a class="btn btn-outline-primary" href="#">Cadastro</a>
+        </div>
+
+        @yield('conteudo')
+    </div>
+</body>
+</html>
